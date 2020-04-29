5299308e5a1bf1d062660457dfd92a6791541da3
Hiago Silverio <10792359+hiagosilverio@users.noreply.github.com>
2020-04-29 00:04:23 -0300

    Added version verify module

--- include/version.bat
+++ include/version.bat
@@ -1,11 +1,15 @@
 @echo off
 
-SET downloadUrl=https://raw.githubusercontent.com/hiagosilverio/redbath/development/README.md
+SET downloadUrl=https://raw.githubusercontent.com/hiagosilverio/redbath/development/redbath.bat
 SET tempFile=%cd%\.%random%-tmp
 
 BITSADMIN /transfer /download %downloadUrl% %tempFile% >nul
-::for /f "delims=" %%x in (%tempFile%) do (set "%%x")
-TYPE %tempFile%
+
+for /f "tokens=2 delims==" %%# in (' type %tempFile%^|find /i "version"') do echo %%#
+
+::https://stackoverflow.com/questions/32188232/print-specific-lines-from-a-batch-file
+
+:: TYPE %tempFile%
 DEL %tempFile%
 
 pause
