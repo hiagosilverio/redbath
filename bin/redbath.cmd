0328f81adc03b0ae963b8161b79f0f320a0bb12f
Hiago Silverio <10792359+hiagosilverio@users.noreply.github.com>
2020-05-11 17:30:37 -0300

    Added new features, changed to cmd extension and clean code

---
+++ bin/redbath.cmd
@@ -0,0 +1,179 @@
+@Echo Off
+setlocal enableextensions 
+setlocal EnableDelayedExpansion
+:start
+cls
+
+: name=Redbath
+: description=Batch Script Reader
+: version=0.1.3
+
+: Constructor
+:_(
+
+  : Call a function by parameter
+  : if first parameter in call bat isn't null
+  if not "%~1" == "" (
+
+    : if second parameter in call bat isn't null
+    if not "%~2" == "" (
+      call:%1 %2
+    )
+
+    : else call first only
+    call:%1
+
+    : return
+    exit /b 0
+
+  ) else (
+      echo Error: parameter invalid or empty function
+  )
+
+  : Calling language file and setting translate variables trought it, doesn't need a function, guess I
+  for /f "delims=" %%x in (%language%\en-US.txt) do (set "%%x")
+
+  :- Global variables
+
+  : Set redbath call alias
+  : => stackoverflow/what-does-dp0-mean-and-how-does-it-work
+  : %~f0 is like full directory that what point out to here ...redbath.cmd
+  set "rd=%~f0"
+
+  : Set language folder 
+  set "language=..\lang"
+
+  : Set source folder 
+  set "source=..\src"
+
+  : Set library folder
+  set "library=..\lib"
+
+  : Set Router
+  set "router=..\src\router.bat"
+
+  : This is where set color.bat path and pass trought it hexadecimal colors
+  : Parameters [hex hexcolor, string messagename] 
+  : Colors for info, sucess and warn
+
+  set "info=%library%\color.bat 0B %*"
+  set "sucess=%library%\color.bat 0A %*"
+  set "warn=%library%\color.bat 0C %*"
+
+  : Set scripts folder
+  set "scripts=..\scripts"
+
+  : Set custom scripts folder
+  set "cscripts=..\custom-scripts"
+
+  : Call wait to set default wait variable
+  call :wait
+
+  : Call main to show the choice options
+  call :menu
+
+: Call constructor itself passing by parameter to answer a call from other file
+: Ex: call ex.bat function value
+) | call:_ %1
+
+: I will let this unofunctional function to get insight about callbacks here and stuff
+: [Deprecated]
+:_wait (
+
+  : Verify if the parameter %~1 (this is like the first param) is null casting to string
+  if not "%~1" == "" (
+     ping 127.0.0.1 -n %1 >nul
+    
+    : this command is to prevent batch exit, calling a function for the second time
+    : it's kinda return
+     exit /b 0
+  ) 
+
+  : return a variable to the global scope, I dunno why it happens.. shrug
+  set "wait=ping 127.0.0.1 -n 1 >nul"
+
+  : and return to constructor (this is to we procedure to continue from construct)
+  exit /b 0
+)
+
+: This wait hack is for when we need to pause the script to read
+:wait (
+
+  : return a variable to the global scope, I dunno why it happens.. shrug
+  set "wait=ping 127.0.0.1 -n 1 >nul"
+
+  : and return to constructor (this is to we procedure to continue from construct)
+  exit /b 0
+)
+
+:menu (
+
+  : Warn if color.bat cannot be read or not exist
+  if not exist "%library%\color.bat" (
+    echo.
+    : I'm not sure why is it here or if it's completed, shrug again
+    echo %WARN_COLOR%
+    echo.
+  )
+
+  : Check updates...
+  : Need to change it to gihub server..
+  : Maybe a function to it?
+  echo Checking internet connection...
+  ping www.google.nl -n 1 -w 1000>nul
+  if errorlevel 1 (echo No internet can't verify update.. skiping) else (
+    echo Connected to the internet..
+    echo.
+  )
+
+  :- Menu
+  : There we define three options to select when someone select 
+  : Like 0,1,2 and that way so on
+
+  call %info% "========[ REDBATH v0.01 ]========"
+  call %info% "------------  Menu --------------" 
+  echo.
+  echo Set in your prompt command consolas 16px font to better experience..
+  echo.
+  call %warn% "Alert: Some scripts may contain dangerous activity"
+  call %warn% "it may could cause harmfull changes,"
+  call %warn% "please verify its content on scripts folder before run"
+  echo.
+  call %warn% "Alert: Do not run any script without batch knowledge," 
+  call %warn% "this software is in development version,"
+  call %warn% "it supports windows 8 and above"
+  echo.
+  call %warn% "Alert: This program not have any responsability for any damage"
+  call %warn% "caused by third scripts, take caution"
+  echo.
+  echo  1 - List scripts
+  echo  2 - List custom scripts [Not working]
+  echo  0 - Exit
+  echo.
+  set /p Comando= %SELECT_OPTION%
+  if "%Comando%" equ "1" (goto op1)
+  if "%Comando%" equ "0" (goto exit)
+
+  : Need to define a config file to disable colors..
+
+  echo.
+  echo %INVALID_OPTION%
+  timeout 1 >nul
+  goto:start
+
+  : Need to transform goto into a fake function...
+  :op1
+  call "%source%\op1.cmd"
+
+  pause>nul
+
+  goto:start
+
+  :exit
+  echo The batch script is being closed...
+  timeout 3 >nul
+
+  echo %ERRORLEVEL%
+  pause
+
+)
--- src/op1.cmd
+++ src/op1.cmd
@@ -1,5 +1,5 @@
-: If not have parameters passing by finish the script
-if not %1 == "op1" ( exit /b)
+: If rd not defined finish the script
+if [%rd%] == [] ( exit /b)
 
 : init first function called main
 : do not call first main like shorthand call:main this could cause trouble during process
@@ -35,23 +35,32 @@ pause
             call %info% "Empty script folder, no script file found"
             %wait%
             echo.
-            set /p question=Do you want to create an example script?
+            : Do not use parentesis in a question (Bug related)
+            set /p scriptQuestion=Do you want to create an example script? [y/n]
             : Maybe a yes or not here..
-            call :ScriptBuild
+            if "%scriptQuestion%" == "Y" ( 
+                call :ScriptBuild
+            ) else (
+                echo no example script
+                call :main
+            )
         )
     ) else (
         call %warn% "Alert: Scripts folder wasn't found in main directory"
         %wait% 
         call %info% "Creating scripts folder..."
         %wait%
-        md scripts
+        : Command to create scripts
+        md %scripts%
         %wait%
+        : Calling ScriptBuild function
         call :ScriptBuild
+        : return to main
+        call :main
     )
     echo.
-    :script
     set /p scriptName=Type the script name: 
-
+    
     if exist ( "%scripts%\%scriptName%.bat" | "%scripts%\%scriptName%" ) (
         echo.
         echo The file was found..
@@ -75,17 +84,21 @@ pause
         call :main
     )
 
-    EXIT /B 0
+    exit /B 0
 )
 
 :ScriptBuild (
 
     call %info% "Inserting script test file.."
     %wait%
-    echo echo batch successfuly executed >> scripts\helloWorld.bat 
-    if %errorlevel% NEQ 0 ( Echo Error: Unknown error on create helloWorld   )
+    : Inserting text into a file named helloWorld.bat
+    echo echo batch successfuly executed >> %scripts%\helloWorld.bat 
+    if NOT %errorlevel% == 0 ( Echo Error: Unknown error on create helloWorld   )
     %wait%
     call %scripts%\helloWorld.bat
 
+    : Call wait from redbath clean the screen, so well there we prevent it with timeout
+    : >nul is a hack to eliminate echo
+    timeout 3 >nul
     EXIT /B 0
 )
