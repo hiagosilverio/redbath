eb865155f807c80df4274ab99fb2460065f5ab96
Hiago Silverio <10792359+hiagosilverio@users.noreply.github.com>
2020-05-14 23:48:45 -0300

    Added new features, custom-scripts and more

--- bin/redbath.cmd
+++ bin/redbath.cmd
@@ -1,27 +1,34 @@
 @Echo Off
 setlocal enableextensions 
 setlocal EnableDelayedExpansion
-: name=Redbath
-: description=Batch Script Reader
-: version=0.1.3
 
-: => Find some batchdoc standards
+@rem title=REDBATH v0.1.5
+@rem description = Batch Script Reader
+@rem version = 0.1.5
 
-: Constructor
+@rem Find some batchdoc standards
+
+@rem  Constructor
+
 :_(
-  : Call a function by parameter
-  : if first parameter in call bat isn't null
+
+  @rem Set current path temporary
+  @rem Need some fix..
+  SET PATH=%PATH%;%~f0
+
+  @rem Call a function by parameter
+  @rem if first parameter in call bat isn't null
   if not "%~1" == "" (
 
-    : if second parameter in call bat isn't null
-    if not "%~2" == "" (
-      call:%1 %2
+    @rem if second parameter in call bat isn't null
+     if not "%~2" == "" (
+      call :%~1 %~2
     )
 
-    : else call first only
-    call:%1
+    @rem else call first only
+    call:%~1
 
-    : return
+    @rem return
     exit /b 0
 
   ) 
@@ -29,109 +36,142 @@ setlocal EnableDelayedExpansion
   : Calling language file and setting translate variables trought it, doesn't need a function, guess I
   for /f "delims=" %%x in (%language%\en-US.txt) do (set "%%x")
 
-  :- Global variables
+  :-Constant variables
 
-  : Set redbath call alias
-  : => stackoverflow/what-does-dp0-mean-and-how-does-it-work
-  : %~f0 is like full directory that what point out to here ...redbath.cmd
-  set "rd=%~f0"
+  @rem  Set redbath call alias
+  @rem  stackoverflow/what-does-dp0-mean-and-how-does-it-work
+  @rem  %~f0 is like full directory that what point out to here ...redbath.cmd
+  @rem  Do not include space between equal and %~f0, with double quotes the result is the same, bug related.
+  set redb=%~f0&
 
-  : Set menu title
-  set title=REDBATH v0.1.3
+  @rem Set menu title
+  set title=REDBATH v0.1.5
 
-  : Set language folder 
+  @rem Set language folder 
   set "language=..\lang"
 
-  : Set source folder 
+  @rem Set source folder 
   set "source=..\src"
 
-  : Set library folder
+  @rem Set library folder
   set "library=..\lib"
 
-  : Set Router
+  @rem Set Router
   set "router=..\src\router.cmd"
 
-  : This is where set color.bat path and pass trought it hexadecimal colors
-  : Parameters [hex hexcolor, string messagename] 
-  : Colors for info, sucess and warn
+  @rem Set scripts folder
+  @rem Overwrite permissions with [%scripts%]
+  set "scripts=[%scripts%]"
+  set "scripts=..\scripts"
+
+  @rem Set custom scripts folder
+  set "cscripts=..\custom-scripts"
+
+  @rem  This is where set color.bat path and pass trought it hexadecimal colors
+  @rem  Parameters [hex hexcolor, string messagename] 
+  @rem  Colors for info, sucess and warn
 
   set "info=%library%\color.cmd 0B %*"
   set "sucess=%library%\color.cmd 0A %*"
   set "warn=%library%\color.cmd 0C %*"
 
-  : Set scripts folder
-  set "scripts=..\scripts"
-
-  : Set custom scripts folder
-  set "cscripts=..\custom-scripts"
-
-  : Call wait to set default wait variable
+  @rem Call wait to set default wait variable
   call :Wait
 
-  : Call main to show the choice options
+  @rem Call main to show the choice options
   call :Main
 
-: Call constructor itself passing by parameter to answer a call from other file
-: Ex: call ex.bat function value
+@rem Call constructor itself passing by parameter to answer a call from other file
+@rem Ex: call ex.bat function value
 ) | call:_ %1
 
-: I will let this unofunctional function to get insight about callbacks here and stuff
-: [Deprecated 0.0.1]
+
+:Sets (
+
+    if not "%~1" == "" (
+
+        @rem prevent spaces with double quotes
+        @rem set variable as parameter1 and value as parameter2
+        @rem No space between = and %~1, %1 
+        set !%~1=%~2!
+
+        exit /b 0
+
+    )
+)
+
+@rem Do not let @get together with @get( the way that works is @get (
+
+:Getas (
+
+    if not "%~1" == "" (
+      
+      @rem Some kind of Witchcraft here
+      @rem !! cast string parameter %~1 to %1% with delayed expansion
+      echo !%1!
+
+      exit /b 0 
+
+    )  
+
+
+)
+
+
+
+@rem I will let this unofunctional function to get insight about callbacks here and stuff
+@rem [Deprecated 0.0.1]
 :_wait (
 
-  : Verify if the parameter %~1 (this is like the first param) is null casting to string
+  @rem Verify if the parameter %~1 (this is like the first param) is null casting to string
   if not "%~1" == "" (
      ping 127.0.0.1 -n %1 >nul
     
-    : this command is to prevent batch exit, calling a function for the second time
-    : it's kinda return
+    @rem this command is to prevent batch exit, calling a function for the second time
+    @rem it's kinda return
      exit /b 0
   ) 
 
-  : return a variable to the global scope, I dunno why it happens.. shrug
+  @rem return a variable to the global scope, I dunno why it happens.. shrug
   set "wait=ping 127.0.0.1 -n 1 >nul"
 
-  : and return to constructor (this is to we procedure to continue from construct)
+  @rem and return to constructor (this is to we procedure to continue from construct)
   exit /b 0
 )
 
-: This wait hack is for when we need to pause the script to read
+@rem This wait hack is for when we need to pause the script to read
 :Wait (
 
-  : return a variable to the global scope, I dunno why it happens.. shrug
-  set "wait=ping 127.0.0.1 -n 1 >nul"
+  @rem return a variable to the global scope, I dunno why it happens.. shrug
+  set "wait=timeout 1 >nul"
 
-  : and return to constructor (this is necessary to continue from construct)
+  @rem and return to constructor (this is necessary to continue from construct)
   exit /b 0
 )
 
-: Show options to be chossen
+@rem Show options to be chossen
 :Main (
 
-  : Clean the prompt screen when nenu is being called
+  @rem Clean the prompt screen when nenu is being called
   cls
   
-  : Warn if color.bat cannot be read or not exist
+  @rem Warn if color.bat cannot be read or not exist
   if not exist "%library%\color.cmd" (
     echo.
-    : I'm not sure why is it here or if it's completed
+    @rem I'm not sure why is it here or if it's completed
     echo %WARN_COLOR%
     echo.
   )
 
-  : Check updates...
-  : Need to change it to gihub server..
-  : Maybe a function to it?
+  @rem Check updates...
+  @rem Need to change it to gihub server..
+  @rem Maybe a function to it?
   echo Checking internet connection...
-  ping www.google.nl -n 1 -w 1000>nul
-  if errorlevel 1 (echo Can't verify update.. skiping) else (
-    echo Connected to the internet..
-    echo.
-  )
-
+  call :CheckConnection
+ 
   :- Menu
-  : There we define three options to select when someone select 
-  : Like 0,1,2 and that way so on
+  @rem There we define three options to select when someone select 
+  @rem Like 0,1,2 and that way so on
 
   call %info% "========[ %title% ]========"
   call %info% "------------  Menu --------------" 
@@ -150,19 +190,20 @@ setlocal EnableDelayedExpansion
   call %warn% "caused by third scripts, take caution"
   echo.
   echo  1 - List scripts
-  echo  2 - List custom scripts [Not working]
+  echo  2 - List custom scripts
   echo  0 - Exit
   echo.
   set /p Command= %SELECT_OPTION%
 
-  : In this case, else need to be in the same line as if
-  : And batch not supports multiple elses, only one follow by if conditions
-  if "%Command%" == "1" ( call :ListScripts ) 
+  @rem In this case, else need to be in the same line as if
+  @rem And batch not supports multiple elses, only one follow by if conditions
+  if "%Command%" == "1" ( call :CallScriptsList ) 
+  if "%Command%" == "2" ( call :CallCustomScriptsList ) 
   if "%Command%" == "0" ( call :Exit )
 
-  : Is there other clean solution for this? like batch script is so limited in variations
-  : Is better to have an exit condition when not passes trought ifs
-  : Than make a boilerplate to consider a solution itself  
+  @rem Is there other clean solution for this? like batch script is so limited in variations
+  @rem Is better to have an exit condition when not passes trought ifs
+  @rem Than make a boilerplate to consider a solution itself  
   
   echo.
   echo %INVALID_OPTION%
@@ -170,17 +211,40 @@ setlocal EnableDelayedExpansion
 
   call :Main
 
-  : End of menu
+  @rem End of menu
+
   )
   
-  : Should define a config file to disable colors.. maybe
+  @rem Should define a config file to disable colors.. maybe
 
-  :ListScripts (
+  :CallScriptsList (
   
-    : Include listscripts cmd into redbath
+    @rem Include listscripts cmd into redbath
     call "%source%\ListScripts.cmd"
     
     call :Main
+    
+    exit /b 0
+  )
+
+  :CallCustomScriptsList (
+    
+    @rem Include listscripts cmd into redbath
+
+    call "%source%\ListScripts.cmd" %cscripts% 
+    
+    call :Main
+    
+    exit /b 0
+    
+  )
+
+  :CheckConnection (
+    
+    ping www.google.nl -n 1 -w 1000>nul
+    if errorlevel 1 (echo Can't verify update.. skiping) else (
+    echo Connected to the internet..
+    echo.
     
     exit /b 0
   )
--- src/ListScripts.cmd
+++ src/ListScripts.cmd
@@ -1,10 +1,41 @@
-: If rd not defined finish the script
-if [%rd%] == [] ( exit /b)
+@Echo Off
+setlocal enableextensions 
+setlocal EnableDelayedExpansion
 
-: init first function called main
-: do not call first main like shorthand call:main this could cause trouble during process
+@rem If rd not defined finish the script
+if [%redb%] == [] ( exit /b)
+
+@rem  init first function called main
+@rem  do not call first main like shorthand call:main this could cause trouble during process
+@rem  Constructor
+
+
+:_(
+
+    @rem Retrieve value passed by parameter as %1
+    @rem Overwrite the scripts variable with parameter
+
+    if not "%~1" == "" (
+        set scripts=%1
+    )
+    
+    @rem Call redbath and push functions with scripts
+    @rem Parameters: [file] [function] [variable] [value]
+    
+    @rem Overwrite problem and set problem, necessary fix
+    : call redbath Sets teste b
+    : call redbath Gets teste
+
+    : echo %scripts%
+
+    call :main
+
+@rem Recieve the variable parameter
+) | call:_ %1 
+
 :main (
 
+    : Overwrite when we declare in batch it means a variable that can recieve two or more values..
     cls
     echo.
     echo Listing script files...
@@ -16,58 +47,70 @@ if [%rd%] == [] ( exit /b)
     : Necessary to avoid loop
     exit /b 0
 
-: Call main as first function
-) | call:main 
+@rem Call main as first function
+) 
 
 
-: get error and return
+@rem get error and return
 echo %ERRORLEVEL%
 pause
 
-: List avaliable scripts
+@rem List avaliable scripts
 :ListScripts (
-
+     
+    @rem Use new scripts variable value into if
     if exist "%scripts%" (
         if exist "%scripts%\*.bat" (
-            :: Listing scripts on scripts folder
+
+            @rem Listing scripts on scripts folder
             dir /b /a-d %scripts%\*.bat | findstr /e .bat | more
+
         ) else (
+            
             call %info% "Empty script folder, no script file found"
             %wait%
             echo.
-            : Do not use parentesis in a question (Bug related)
+            
+            @rem Do not use parentesis in a question (Bug related)
             set /p scriptQuestion=Do you want to create an example script? [y/n]
-            : Maybe a yes or not here..
+
+            @rem Maybe a yes or not here..
             if "%scriptQuestion%" == "Y" ( 
                 call :ScriptBuild
-            ) else (
-                echo no example script
+            ) else (     
                 call :main
             )
+
         )
     ) else (
+
         call %warn% "Alert: Scripts folder wasn't found in main directory"
         %wait% 
         call %info% "Creating scripts folder..."
         %wait%
-        : Command to create scripts
+        @rem Command to create scripts
         md %scripts%
         %wait%
         
-        : Calling ScriptBuild function
+        @rem Calling ScriptBuild function
         call :ScriptBuild
 
-        : return to main
+        @rem return to main
         call :main
+
     )
     echo.
     set /p scriptName=Type the script name: 
     
     if exist ( "%scripts%\%scriptName%.bat" | "%scripts%\%scriptName%" ) (
+
         echo.
         echo The file was found..
         %wait%
         call %info% "To force stop the batch processing press CRTL+C"
+        echo.
+        call %info% "Please, do not close the window or turn off the computer between disk formatation, copy or move"
+
         echo.
         echo Running script..
         timeout 3 >nul
@@ -79,12 +122,15 @@ pause
         echo Backing to scripts menu..
         timeout 6 >nul
         call :main 
+
     ) else (
+
         echo.
         call %warn% "Alert: Invalid info or bad typing: %scriptName%"
         echo Redirecting to the script listing.. 
         timeout 6 >nul
         call :main
+
     )
 
     exit /B 0
@@ -94,14 +140,23 @@ pause
 
     call %info% "Inserting script test file.."
     %wait%
-    : Inserting text into a file named helloWorld.bat
+    @rem Inserting text into a file named helloWorld.bat
     echo echo batch successfuly executed >> %scripts%\helloWorld.bat 
     if NOT %errorlevel% == 0 ( Echo Error: Unknown error on create helloWorld   )
     %wait%
     call %scripts%\helloWorld.bat
 
-    : Call wait from redbath clean the screen, so well there we prevent it with timeout
-    : >nul is a hack to eliminate echo
+    @rem Call wait from redbath clean the screen, so well there we prevent it with timeout
+    @rem >nul is a hack to eliminate echo
     timeout 3 >nul
     EXIT /B 0
+
+)
+
+:Redirect (
+
+    echo.
+    echo Backing to scripts menu..
+    timeout 6 >nul
+
 )
